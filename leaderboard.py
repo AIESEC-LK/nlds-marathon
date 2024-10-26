e96ef6f724845d6c9279fb058e803f49d8ab26a8
Pasindu Bhanuka <86610640+bhanusunrise@users.noreply.github.com>
2024-10-26 16:13:35 +0530

    Font Size 2

--- leaderboard.py
+++ leaderboard.py
@@ -158,11 +158,11 @@ def applied_bar_chart_and_data(data):
 
     # Hide the legend
     fig_applied.update_layout(
-    title_font=dict(size=20),  # Title font size
-    xaxis_title_font=dict(size=16),  # X-axis title font size
-    yaxis_title_font=dict(size=16),  # Y-axis title font size
-    xaxis_tickfont=dict(size=14),  # X-axis tick font size
-    yaxis_tickfont=dict(size=14),  # Y-axis tick font size
+    title_font=dict(size=30),  # Title font size
+    xaxis_title_font=dict(size=26),  # X-axis title font size
+    yaxis_title_font=dict(size=26),  # Y-axis title font size
+    xaxis_tickfont=dict(size=24),  # X-axis tick font size
+    yaxis_tickfont=dict(size=24),  # Y-axis tick font size
     showlegend=False
 )
 
@@ -180,11 +180,11 @@ def approved_bar_chart_and_data(data):
     fig_approved = px.bar(df_entity_approved_total, x='Entity', y='Total_Approved', title='Total Approvals by Entity', labels={'Entity': 'Entity', 'Total_Approved': 'Approvals'},color='Entity')
     # Hide the legend
     fig_approved.update_layout(
-    title_font=dict(size=20),  # Title font size
-    xaxis_title_font=dict(size=16),  # X-axis title font size
-    yaxis_title_font=dict(size=16),  # Y-axis title font size
-    xaxis_tickfont=dict(size=14),  # X-axis tick font size
-    yaxis_tickfont=dict(size=14),  # Y-axis tick font size
+    title_font=dict(size=30),  # Title font size
+    xaxis_title_font=dict(size=26),  # X-axis title font size
+    yaxis_title_font=dict(size=26),  # Y-axis title font size
+    xaxis_tickfont=dict(size=24),  # X-axis tick font size
+    yaxis_tickfont=dict(size=24),  # Y-axis tick font size
     showlegend=False
 )
 
