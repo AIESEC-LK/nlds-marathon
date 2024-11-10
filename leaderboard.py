34061b6df69f86e58fa7b3ea5ecda17784f91437
Dinuka Avinash <e19029@eng.pdn.ac.lk>
2024-11-10 23:01:44 +0530

    bug fix - renaming

--- leaderboard.py
+++ leaderboard.py
@@ -338,7 +338,7 @@ def display_leaderboard_table(df):
     # Specify the order of columns explicitly
     # Make sure that the columns listed here match your DataFrame
     columns_order = ['Rank', 'Entity', 'OPS Score',
-                     'Total Applications', 'Total Approvals', 'Applied to Approved Ratio']
+                     'Total Applications', 'Total Approvals', 'Applied to Approved Ratio %']
 
     # Check if all specified columns exist in the DataFrame
     for col in columns_order:
